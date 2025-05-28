450ac06dce61d5c43f91c438ed2c737f490d0cd7
VourltazScripSkid <110595000+VourltazScriptSkid@users.noreply.github.com>
2025-05-28 23:50:24 +1000

    4.2C

--- packages/open_loop_square/src/open_loop_square.py
+++ packages/open_loop_square/src/open_loop_square.py
@@ -1,33 +1,34 @@
 #!/usr/bin/env python3
 
 import rospy
-from duckietown_msgs.msg import Twist2DStamped
-from duckietown_msgs.msg import FSMState
-from duckietown_msgs.msg import WheelEncoderStamped
-import math
+from duckietown_msgs.msg import Twist2DStamped, FSMState, WheelEncoderStamped
 
-class Drive_Square:
+class DriveSquare:
     def __init__(self):
         rospy.init_node('drive_square_node', anonymous=True)
-        
-        # Choose the test mode here
-        self.test_mode = "square_floor_1"  # Change to: "straight_0.2", "rotate_1.0", etc.
+
+        # === CHANGE THIS FOR DIFFERENT TEST VIDEOS ===
+        self.test_mode = "square_floor_1"
 
         self.cmd_msg = Twist2DStamped()
         self.left_ticks = 0
         self.right_ticks = 0
 
-        self.TICKS_PER_METER = 738
-        self.TICKS_PER_90_DEG = 90
+        # Based on your measurements:
+        self.TICKS_PER_METER = (738 + 738) / 2
+        self.TICKS_PER_90_DEG = (90 + 90) / 2
 
         self.pub = rospy.Publisher('/stripe/car_cmd_switch_node/cmd', Twist2DStamped, queue_size=1)
         rospy.Subscriber('/stripe/fsm_node/mode', FSMState, self.fsm_callback, queue_size=1)
         rospy.Subscriber('/stripe/left_wheel_encoder_node/tick', WheelEncoderStamped, self.left_encoder_callback)
         rospy.Subscriber('/stripe/right_wheel_encoder_node/tick', WheelEncoderStamped, self.right_encoder_callback)
 
+        self.received_encoder_data = False
+
     def fsm_callback(self, msg):
-        if msg.state == "LANE_FOLLOWING":
-            rospy.sleep(1)
+        if msg.state == "LANE_FOLLOWING" and not self.received_encoder_data:
+            self.received_encoder_data = True
+            rospy.sleep(1.0)
             self.run_demo()
 
     def left_encoder_callback(self, msg):
@@ -42,36 +43,59 @@ class Drive_Square:
         self.cmd_msg.omega = 0.0
         self.pub.publish(self.cmd_msg)
 
-    def move_straight(self, distance, speed):
-        ticks_needed = abs(distance * self.TICKS_PER_METER)
-        initial_ticks = self.left_ticks
-        rate = rospy.Rate(60)
-        direction = 1 if distance > 0 else -1
+    def move_straight(self, distance_m, speed_mps):
+        direction = 1 if distance_m > 0 else -1
+        speed_mps = abs(speed_mps) * direction
 
-        while abs(self.left_ticks - initial_ticks) < ticks_needed:
-            self.cmd_msg.v = speed * direction
+        initial_left = self.left_ticks
+        initial_right = self.right_ticks
+
+        ticks_needed = abs(distance_m) * self.TICKS_PER_METER
+        rate = rospy.Rate(60)
+
+        while not rospy.is_shutdown():
+            delta_left = self.left_ticks - initial_left
+            delta_right = self.right_ticks - initial_right
+            avg_ticks = (delta_left + delta_right) / 2.0
+
+            if abs(avg_ticks) >= ticks_needed:
+                break
+
+            self.cmd_msg.v = speed_mps
             self.cmd_msg.omega = 0.0
             self.cmd_msg.header.stamp = rospy.Time.now()
             self.pub.publish(self.cmd_msg)
             rate.sleep()
 
         self.stop_robot()
+        rospy.sleep(0.5)  # Delay after motion
 
-    def rotate_in_place(self, angle_deg, speed):
-        ticks_needed = abs((angle_deg / 90.0) * self.TICKS_PER_90_DEG)
+    def rotate_in_place(self, angle_deg, angular_speed):
+        direction = 1 if angle_deg > 0 else -1
+        angular_speed = abs(angular_speed) * direction
+
         initial_left = self.left_ticks
         initial_right = self.right_ticks
-        rate = rospy.Rate(60)
-        direction = 1 if angle_deg > 0 else -1
 
-        while abs((self.right_ticks - initial_right) - (self.left_ticks - initial_left)) < ticks_needed:
+        ticks_needed = abs(angle_deg) / 90.0 * self.TICKS_PER_90_DEG
+        rate = rospy.Rate(60)
+
+        while not rospy.is_shutdown():
+            delta_left = self.left_ticks - initial_left
+            delta_right = self.right_ticks - initial_right
+            diff_ticks = abs(delta_right - delta_left)
+
+            if diff_ticks >= ticks_needed:
+                break
+
             self.cmd_msg.v = 0.0
-            self.cmd_msg.omega = speed * direction
+            self.cmd_msg.omega = angular_speed
             self.cmd_msg.header.stamp = rospy.Time.now()
             self.pub.publish(self.cmd_msg)
             rate.sleep()
 
         self.stop_robot()
+        rospy.sleep(0.5)  # Delay after rotation
 
     def draw_square(self, speed_linear, speed_angular):
         for _ in range(4):
@@ -79,17 +103,18 @@ class Drive_Square:
             self.rotate_in_place(90, speed_angular)
 
     def run_demo(self):
-        if self.test_mode == "straight_0.2":
+        mode = self.test_mode
+        if mode == "straight_0.2":
             self.move_straight(1.0, 0.2)
-        elif self.test_mode == "straight_0.4":
+        elif mode == "straight_0.4":
             self.move_straight(1.0, 0.4)
-        elif self.test_mode == "rotate_1.0":
+        elif mode == "rotate_1.0":
             self.rotate_in_place(90, 1.0)
-        elif self.test_mode == "rotate_2.0":
+        elif mode == "rotate_2.0":
             self.rotate_in_place(90, 2.0)
-        elif self.test_mode == "square_floor_1":
+        elif mode == "square_floor_1":
             self.draw_square(0.2, 1.0)
-        elif self.test_mode == "square_floor_2":
+        elif mode == "square_floor_2":
             self.draw_square(0.2, 1.0)
         else:
             rospy.logwarn("Invalid test mode selected.")
@@ -100,7 +125,7 @@ class Drive_Square:
 
 if __name__ == '__main__':
     try:
-        controller = Drive_Square()
-        controller.run()
+        node = DriveSquare()
+        node.run()
     except rospy.ROSInterruptException:
         pass
